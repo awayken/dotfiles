6b5a70f1b81ded0d0f5d8192b939053bd5131148
Miles Rausch <miles@milesrausch.com>
2013-03-28 11:08:37 -0500

    Create Microsoft.PowerShell_profile.ps1
    From http://markembling.info/2009/09/my-ideal-powershell-prompt-with-git-integration

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,39 @@
+# Alias
+Set-Alias subl 'c:\program files\sublime text 2\sublime_text.exe'
+
+# Displays git branch and stats when inside a git repository.
+# http://markembling.info/2009/09/my-ideal-powershell-prompt-with-git-integration
+
+. (Resolve-Path ~/Documents/WindowsPowershell/gitutils.ps1)
+
+function prompt {
+    $userLocation = $pwd
+    $host.UI.RawUi.WindowTitle = $userLocation
+    Write-Host($userLocation) -nonewline
+
+    if (isCurrentDirectoryGitRepository) {
+        $status = gitStatus
+        $currentBranch = $status["branch"]
+
+        Write-Host(' [') -nonewline
+        Write-Host($currentBranch) -nonewline -foregroundcolor Yellow
+        if ($status["ahead"] -ne $FALSE) {
+            # We are ahead of origin
+            Write-Host(' (ahead)') -nonewline -foregroundcolor Magenta
+        }
+
+        Write-Host(' +' + $status["added"]) -nonewline -foregroundcolor Green
+        Write-Host(' ~' + $status["modified"]) -nonewline -foregroundcolor Red
+        Write-Host(' -' + $status["deleted"]) -nonewline -foregroundcolor Red
+
+        if ($status["untracked"] -ne $FALSE) {
+            Write-Host(' !') -nonewline -foregroundcolor Magenta
+        }
+
+        Write-Host(']>') -nonewline
+    } else {
+        Write-Host('>') -nonewline
+    }
+
+    return " "
+}
